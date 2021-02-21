58b730e66b086985f853c475959986260df7a804
Bradley Willimack <brad.willimack@gmail.com>
2021-02-20 21:50:42 -0600

    added tile 4 image to list of states in tile class

--- mytile.py
+++ mytile.py
@@ -4,7 +4,8 @@ class MyTile():
     def __init__(self, x_pos, y_pos, tile_size):
         self.possible_states = [pygame.transform.scale(pygame.image.load("example_tile_image.png"), (tile_size,tile_size)), 
                                 pygame.transform.scale(pygame.image.load("example_tile_image2.png"), (tile_size,tile_size)),
-                                pygame.transform.scale(pygame.image.load("example_tile_image3.png"), (tile_size,tile_size)),]
+                                pygame.transform.scale(pygame.image.load("example_tile_image3.png"), (tile_size,tile_size)),
+                                pygame.transform.scale(pygame.image.load("example_tile_image4.png"), (tile_size,tile_size)),]
         self.current_state = 0
         self.x_pos = x_pos
         self.y_pos = y_pos
